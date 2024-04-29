1583c9fd40bb3de335daa842aea1e1e30853d674
nhannv-zigexn <nhannv@zigexn.vn>
2024-04-29 12:39:43 +0700

    Update Jenkinsfile

--- deploy/pm2-check.sh
+++ deploy/pm2-check.sh
@@ -4,7 +4,7 @@ PM2_EXIST=$(if pm2 list 2> /dev/null | grep -q blog-nvnhan0810; then echo "Yes" 
 
 if [ $PM2_EXIST = Yes ] ; then
     pm2 stop blog-nvnhan0810
-    pm2 delte blog-nvnhan0810
+    pm2 delete blog-nvnhan0810
 fi
 
 pm2 --name blog-nvnhan0810 start npm -- start
