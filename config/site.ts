c8e09331d4a4d45c3a6e83ccc052dd17f674cd2a
nhannv-zigexn <nhannv@zigexn.vn>
2024-06-04 21:59:16 +0700

    Update github link

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   author: "Nguyen Van Nhan",
   email: "nguyenvannhan0810@gmail.com",
   links: {
-    github: "https://github.com/nguyenvannhan0810",
+    github: "https://github.com/nguyenvannhan",
     linkedin: "https://www.linkedin.com/in/nguyen-van-nhan-754052148/",
     personalSite: "https://nvnhan0810.com",
   }
