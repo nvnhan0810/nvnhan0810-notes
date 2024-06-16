df631335b1728df27289652ce820ed3e4171edf1
Nguyen Van Nhan <nguyenvannhan0810@gmail.com>
2024-06-16 17:26:36 +0700

    Update Logout

--- components/auth-dropdown.tsx
+++ components/auth-dropdown.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LogOutIcon } from "lucide-react";
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
@@ -9,7 +10,9 @@ const AuthDropdown = ({ auth }: { auth: Session | null }) => {
         <DropdownMenu>
             <DropdownMenuTrigger>{auth.user.name}</DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuItem onClick={() => signOut()}> 123
+                <DropdownMenuItem onClick={() => signOut()}> 
+                    <LogOutIcon className="mr-2 h-4 w-4" />
+                    Logout
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
