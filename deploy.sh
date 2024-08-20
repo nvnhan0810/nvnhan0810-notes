c7a5383b74e9f19aa7d1bf39645e3ac3822c8eb4
nguyenvannhan <nguyenvannhan0810@gmail.com>
2024-08-20 12:33:00 +0700

    Remove prisma

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+set -e
+
+fnm use
+
+npm install -g pnpm
+
+pnpm install
+
+pnpm run build
+
+npm i -g pm2
+
+PM2_EXIST=$(if pm2 list 2>/dev/null | grep -q nvnahn0810-notes; then echo "Yes"; else echo "No"; fi)
+
+if [ $PM2_EXIST = Yes ]; then
+	pm2 stop nvnahn0810-notes
+	pm2 delete nvnahn0810-notes
+fi
+
+pm2 --name nvnahn0810-notes start pnpm -- start
+
