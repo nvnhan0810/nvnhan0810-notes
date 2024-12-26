94e56e783f6d401436c7cecc4f76b9fcd1b16fb6
Van Nhan <nguyenvannhan0810@gmail.com>
2024-12-27 01:17:41 +0700

    Post: laravel github actions vps

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ pnpm run build
 
 npm i -g pm2
 
-PM2_EXIST=$(if pm2 list 2>/dev/null | grep -q nvnahn0810-notes; then echo "Yes"; else echo "No"; fi)
+PM2_EXIST=$(if pm2 list 2>/dev/null | grep -q nvnhan0810-notes; then echo "Yes"; else echo "No"; fi)
 
 if [ $PM2_EXIST = Yes ]; then
-	pm2 stop nvnahn0810-notes
-	pm2 delete nvnahn0810-notes
+	pm2 stop nvnhan0810-notes
+	pm2 delete nvnhan0810-notes
 fi
 
-pm2 --name nvnahn0810-notes start pnpm -- start -p 3002
+pm2 --name nvnhan0810-notes start pnpm -- start -p 3002
 
