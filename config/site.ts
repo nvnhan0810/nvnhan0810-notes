577be7d2d68570196fee725685d77ebf41b559eb
thk-vannhan <n_vannhan@thk-hd.vn>
2025-06-23 16:25:04 +0700

    Update site info

--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
-  name: "Notes",
-  url: "https://notes.nvnhan0810.com",
+  name: "Blogs",
+  url: "https://it.nvnhan0810.com",
   description: "The site which I note the interest something to save... ::))",
   author: "Nguyen Van Nhan",
   email: "nguyenvannhan0810@gmail.com",
   links: {
-    github: "https://github.com/nguyenvannhan",
+    github: "https://github.com/nvnhan0810",
     linkedin: "https://www.linkedin.com/in/nguyen-van-nhan-754052148/",
     personalSite: "https://nvnhan0810.com",
   }
